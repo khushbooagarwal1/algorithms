ec73cf09afb0fcb2ce0b5b9c59a13d050abb0526
khushbooagarwal1 <khushbooagarwal503@gmail.com>
2017-06-15 21:52:20 +0530

    Create kruskal.cpp

--- graph/Kruskal -MST- Really Special Subtree/kruskal.cpp
+++ graph/Kruskal -MST- Really Special Subtree/kruskal.cpp
@@ -11,6 +11,7 @@ struct comp{
     }
 };
 int find(int a, vector< int > & parent){
+    //find parent of each set
     if(parent[a] == -1){
         return a;
     }
@@ -19,6 +20,7 @@ int find(int a, vector< int > & parent){
     }
 }
 void union_edge(pair< int, int > edge, vector< int > & parent){
+    //make parent of one set point to another
     int x = find(edge.first, parent);
     
     int y = find(edge.second, parent);
@@ -28,6 +30,7 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n, m;
     cin >> n >> m;
+    //min heap storing <weight, edge(node, node) > sorted on weight 
     priority_queue< pair<int, pair< int, int > >, vector< pair<int, pair< int, int > > >, comp > pq;
     for(int i = 0; i < m; i++){
         int a, b, c;
@@ -35,10 +38,13 @@ int main() {
         pq.push(make_pair(c, make_pair(a-1, b-1)));
     }
     int res = 0;
-    vector< int > parent(n, -1);
+    vector< int > parent(n, -1);    //store parent node for union and find operation
+    
     while(!pq.empty()){
+        //pop minimum weight edge
         int weight = pq.top().first;
         pair< int, int > edge = pq.top().second;
+        //if belong to differnt set, combine them
         if(find(edge.first, parent) != find(edge.second, parent)){
             union_edge(edge, parent);
             res += weight;
