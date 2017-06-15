e7135cf3cd7215819f5957b25361cf580502e5a3
khushbooagarwal1 <khushbooagarwal503@gmail.com>
2017-06-15 21:46:49 +0530

    Create bfs.cpp

--- graph/Breadth First Search- Shortest Reach/bfs.cpp
+++ graph/Breadth First Search- Shortest Reach/bfs.cpp
@@ -7,24 +7,30 @@
 #include <algorithm>
 using namespace std;
 void bfs(map<int, vector< int > > adj, int s, int n){
-    vector< int > visited(n, 0);
-    vector< int > dist(n, -1);
-    queue< pair< int,int > > q;
-    q.push(make_pair(s, 0));
+  
+    vector< int > visited(n, 0);    //to check for visited nodes
+    vector< int > dist(n, -1);      //distance from source node. 
+    queue< pair< int,int > > q;     //to store node along with their distance
+    //for source node
+    q.push(make_pair(s, 0));        
     visited[s] = 1;
     dist[s] = 0;
     while(!q.empty()){
+        //extract each node from queue and process
         int u = q.front().first;
         int pos = q.front().second; 
         q.pop();
+        //get all adjacent nodes and add to queue
         for(int i = 0 ; i < adj[u].size(); i++){
             if(visited[adj[u][i]] == 0){
                 q.push(make_pair(adj[u][i], pos+1));
                 visited[adj[u][i]] = 1;
+                //each edge weight is 6, so multiplied by 6
                 dist[adj[u][i]] = 6* (pos+1);
             }
         }
     }
+    //print result
     for(int i = 0; i < n; i++){
         if(i != s){
             if(i == n-1){
@@ -48,6 +54,7 @@ int main() {
         for(int i = 0; i < m; i++){
             int temp1, temp2;
             cin >> temp1 >> temp2;
+            //adjacency matrix
             adj[temp1-1].push_back(temp2-1);
             adj[temp2-1].push_back(temp1-1);
         }
